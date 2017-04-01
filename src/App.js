4c5b1b61337212ce7f19b2488d9f8e469054a712
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-04-01 16:48:37 -0500

    Implementación de Firebase y autenticación Google
    * Lo primero que debemos hacer es instalar firebase en nuestro proyecto,
    a través de npm
    
       npm install --save firebase
    
    * Copiar los datos de configuración de la consola de firebase al
    index.js de la aplicación.
    
    Importamos la loibrería de firebase en index.js y app.js
    Implementamos los métodos de autenticar y salir, creamos un constructor
    para saber el estado del usuario y llamamos los métodos proveidos por la
    librería de firebase en el archivo app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase';
 import './App.css';
 
 class App extends Component {
+
+  constructor () {
+    super();
+    this.state = {
+      user: null
+    };
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  componentWillMount () {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user });
+    });
+  }
+
+  handleAuth () {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} Ha iniciado Sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  handleLogout ()  {
+    firebase.auth().signOut()
+    .then(result => console.log(`${result.user.email} Ha Salido`))
+    .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  renderLoginButton () {
+    // Si usuario está logueado
+    if (this.state.user){
+      return (
+       <div>
+        <img src={this.state.user.photoURL} alt={this.state.user.displayName} width={100} height={100}/>
+        <p>Hola {this.state.user.displayName}!</p>
+        <button onClick={this.handleLogout}>Salir</button>
+       </div>
+      );
+    } else {
+      return (
+        <button onClick={this.handleAuth}>Login Con Google</button>
+      );
+    }
+    //Si no está logueado
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Pseudogram</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.renderLoginButton()}
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+
 import App from './App';
 import './index.css';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBaDBqTJTR8QztStwLir_yEvpKUCDh9sAM",
+  authDomain: "pseudogram-fab9a.firebaseapp.com",
+  databaseURL: "https://pseudogram-fab9a.firebaseio.com",
+  projectId: "pseudogram-fab9a",
+  storageBucket: "pseudogram-fab9a.appspot.com",
+  messagingSenderId: "666737739828"
+});
 
 ReactDOM.render(
   <App />,
