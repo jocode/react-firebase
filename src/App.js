a3ee04b06a1393d036039ab353c8527d0d4a91fb
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-04-01 19:08:58 -0500

    Almacenar datos con Firebase Database RealTime
    Vamos a almacenar datos de la imagen y del usuario en la base de datos
    de firebase.
    * La funcionalidad de la suboda de archivos la realizamos en App.js en
    vez del archivo FileUpload.js
    
    * Usamos el objeto database de firebase y usamos los métodos necesarios
    para insertar los datos de la imagen
    * Almacenamos las imagenes en un array para luego renderizarlas en la
    vista, se usa el método map para que no se reemplacen los datos si no
    que almacene los anteriores y me agregue los siguiente y revese para
    iterarlos desde el ultimo hasta el primero.
    
    Para poner ejecutarel proyecto ubicarse en la ruta de la carpeta con la
    consola y ejecutar el comando de node   [npm start]  para iniciar l
    servidor.

--- src/App.js
+++ src/App.js
@@ -9,15 +9,24 @@ class App extends Component {
   constructor () {
     super();
     this.state = {
-      user: null
+      user: null,
+      pictures: []
     };
     this.handleAuth = this.handleAuth.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
+    this.handleUpload = this.handleUpload.bind(this);
   }
 
+  /*Componente del ciclo de vida de las vistas de react*/
   componentWillMount () {
     firebase.auth().onAuthStateChanged(user => {
       this.setState({ user });
+    });
+
+    firebase.database().ref('pictures').on('child_added', snapshot => {
+      this.setState({
+        pictures: this.state.pictures.concat(snapshot.val())
+      });
     });
   }
 
@@ -34,6 +43,32 @@ class App extends Component {
     .catch(error => console.log(`Error ${error.code}: ${error.message}`));
   }
 
+  handleUpload (event) {
+    //Obtenermos el fichero en el evento onChange
+    const file = event.target.files[0];
+    const storageRef = firebase.storage().ref(`/fotos/${file.name}`);
+    const task = storageRef.put(file);
+
+    // Obtenemos el estado del fichero al subir la imagenes a firebase
+    task.on('state_changed', snapshot => {
+      let percentaje = (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+      this.setState({
+        uploadValue: percentaje
+      });
+    }, error => { console.log(error.message)
+    }, () => {
+        const record = {
+          photoURL: this.state.user.photoURL,
+          displayName: this.state.user.displayName,
+          image: task.snapshot.downloadURL
+        };
+
+        const dbRef = firebase.database().ref('pictures');
+        const newPicture = dbRef.push();
+        newPicture.set(record);
+    });
+  }
+
   renderLoginButton () {
     // Si usuario está logueado
     if (this.state.user){
@@ -42,7 +77,16 @@ class App extends Component {
         <img src={this.state.user.photoURL} alt={this.state.user.displayName} width="100"/>
         <p>Hola {this.state.user.displayName}!</p>
         <button onClick={this.handleLogout}>Salir</button>
-        <FileUpload />
+        <FileUpload onUpload={this.handleUpload}/>
+        {
+          this.state.pictures.map(picture => (
+            <div>
+              <img src={picture.image}/><br/>
+              <img width="50" src={picture.photoURL} alt={picture.displayName}/><br/>
+              <span>{picture.displayName}</span>
+            </div>
+          )).reverse()
+        }
        </div>
       );
     } else {
--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,36 +1,12 @@
 import React, { Component } from 'react';
-import firebase from 'firebase';
 
 class FileUpload extends Component {
 
   constructor () {
     super();
     this.state = {
-      uploadValue: 0,
-      picture: null
+      uploadValue: 0
     };
-    this.handleUpload = this.handleUpload.bind(this);
-  }
-
-  handleUpload (event) {
-    //Obtenermos el fichero en el evento onChange
-    const file = event.target.files[0];
-    const storageRef = firebase.storage().ref(`/fotos/${file.name}`);
-    const task = storageRef.put(file);
-
-    // Obtenemos el estado del fichero al subir la imagenes a firebase
-    task.on('state_changed', snapshot => {
-      let percentaje = (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
-      this.setState({
-        uploadValue: percentaje
-      });
-    }, error => { console.log(error.message)
-    }, () => {
-      this.setState({
-        uploadValue: 100,
-        picture: task.snapshot.downloadURL
-      });
-    });
   }
 
   render (){
@@ -38,9 +14,7 @@ class FileUpload extends Component {
       <div>
         <progress value={this.state.uploadValue} max="100"></progress>
         <br/>
-        <input type="file" onChange={this.handleUpload}/>
-        <br/>
-        <img width="320" src={this.state.picture} alt=""/>
+        <input type="file" onChange={this.props.onUpload}/>
       </div>
     );
   }
